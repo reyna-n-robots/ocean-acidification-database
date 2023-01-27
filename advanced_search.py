558f5687b67fdc8538b8f937d69ee8e3edbd026b
reyna-n-robots <reyna.j.ayala@gmail.com>
2023-01-27 16:59:18 -0500

    Single result data collection
    Need to resolve procedural issues commented under "collect_single_result"

--- advanced_search.py
+++ advanced_search.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import xlsxwriter
 
 import page
 import elemnt
@@ -23,10 +27,32 @@ class Advanced_Search(unittest.TestCase):
         self.assertTrue(self.search_results_page.is_results_found(), "No results found.")
 
     def open_single_result(self, row_num):
-        base_page_element = elemnt.BasePageElement()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'MainRepeaterDetailRow_'+row_num)))
         row_i = self.driver.find_element(By.ID, 'MainRepeaterDetailRow_' + row_num)
-        link_i = row_i.find_element(By.TITLE, 'Click to view complete record')
+        link_i = row_i.find_element(By.CLASS_NAME, 'NoVisitNoUnder')
         link_i.click()
+    
+    def collect_single_result(self, row_num):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_webopacContentHolder_detailContent_BibliographicDetail_BibDetailRepeater_ctl01_DataCell')))
+
+        entry = [0] * 13
+        
+        # works manually for now but will have to change to: stop reading when you hit "notes" --> break
+        # if labelcell text is blank then append to str not list -- this only needs to be applied to author names
+        # if it's just numbers and not any other characters, convert back to int or float
+        for i in range(1,14):
+            if i < 10:
+                entry[i-1] = self.driver.find_element(By.ID, 'ctl00_ctl00_webopacContentHolder_detailContent_BibliographicDetail_BibDetailRepeater_ctl0'+ str(i) +'_DataCell').text
+            else:
+                entry[i-1] = self.driver.find_element(By.ID, 'ctl00_ctl00_webopacContentHolder_detailContent_BibliographicDetail_BibDetailRepeater_ctl'+ str(i) +'_DataCell').text
+        
+        print(entry)
+
+        dataframe = pd.DataFrame([entry])
+        writer = pd.ExcelWriter('database.xlsx', engine='xlsxwriter')
+        dataframe.to_excel(writer, sheet_name='Sheet1', startrow=row_num+1, index=False, header=False)
+        writer.close()
+
 
     def tearDown(self):
         self.driver.close()
@@ -38,4 +64,5 @@ search = Advanced_Search()
 search.set_up()
 search.search_ocean_acidif()
 search.open_single_result('1')
+search.collect_single_result(1)
 #search.tearDown()
--- page.py
+++ page.py
@@ -1,5 +1,5 @@
-from element import BasePageElement
-from locators import MainPageLocators
+from elemnt import BasePageElement
+#from locators import MainPageLocators
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
